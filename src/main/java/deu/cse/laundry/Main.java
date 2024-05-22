e31e21f80b2b53d9230c7fdd6a4a6ba144305b56
young0306 <150253108+young0306@users.noreply.github.com>
2024-05-22 09:43:57 +0900

    Update Main.java

--- src/main/java/deu/cse/laundry/Main.java
+++ src/main/java/deu/cse/laundry/Main.java
@@ -11,11 +11,12 @@ public class Main {
 
     public static void main(String[] args) {
         // WashingMachine 인스턴스를 얻습니다.
-        WashingMachine washingMachine1 = WashingMachine.getInstance();
-        WashingMachine washingMachine2 = WashingMachine.getInstance();
+        WM washingMachine1 = WM.getInstance();
+        WM washingMachine2 = WM.getInstance();
 
         // 두 인스턴스가 동일한지 확인하여 싱글톤 패턴이 올바르게 작동하는지 검증합니다.
         System.out.println(washingMachine1 == washingMachine2); // true가 출력되어야 합니다.
     }
 }
+
 
