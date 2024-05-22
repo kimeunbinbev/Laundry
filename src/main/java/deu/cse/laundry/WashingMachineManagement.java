aabf60a7f604c7e1e37230c9f0881f854b41a0d2
young0306 <150253108+young0306@users.noreply.github.com>
2024-05-23 02:24:11 +0900

    Update WashingMachineManagement.java

--- src/main/java/deu/cse/laundry/WashingMachineManagement.java
+++ src/main/java/deu/cse/laundry/WashingMachineManagement.java
@@ -100,6 +100,7 @@ public class WashingMachineManagement extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
