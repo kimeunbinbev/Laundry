c2483bdda5368939ed1e8c9048c186da8d407fa8
young0306 <150253108+young0306@users.noreply.github.com>
2024-05-23 02:22:47 +0900

    Update WashingMachineManagement.java

--- src/main/java/deu/cse/laundry/WashingMachineManagement.java
+++ src/main/java/deu/cse/laundry/WashingMachineManagement.java
@@ -100,13 +100,6 @@ public class WashingMachineManagement extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-            if (jButton2.isSelected()) {
-            // 버튼이 선택된 상태일 때 JList에 아이템 추가
-            listModel.addElement(jButton2.getText());
-        } else {
-            // 버튼이 선택되지 않은 상태일 때 JList에서 아이템 삭제
-            listModel.removeElement(jButton2.getText());
-        }
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
